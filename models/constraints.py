689dd7a0bd77c71790eb298510573a621c390c46
JiaqiBai123 <baijiaqi123@sjtu.edu.cn>
2026-08-17 02:30:28 +0800

    Update constraints

--- models/constraints.py
+++ models/constraints.py
@@ -44,7 +44,7 @@ def add_all_constraints(
         add_weight_floor_constraint(model, graph, options, walks)
  
     if options.enforce_connectivity:
-        add_connectivity(model, graph, walks)
+        add_connectivity_constraint(model, graph, walks)
     else:
         logger.debug(
             "Connectivity enforcement disabled; walks may decompose into "
@@ -76,10 +76,50 @@ def add_capacity_constraints(
     """
     Walks cannot send more copy number through an edge than it has left.
     """
-    # TBD - see "1. capacity" in CE paper, also need to consider multiple cycles/walks for MIN_CYCLES
-    #
-    #
+    model.EdgeCapacity = pyo.ConstraintList()
+    edge_keys = list(graph.edge_keys())
 
+    def capacity(key: EdgeId) -> float | None:
+        if key.type == EdgeType.SEQUENCE:
+            return residual_cn.sequence[key.idx]
+        if key.type == EdgeType.CONCORDANT:
+            return residual_cn.concordant[key.idx]
+        if key.type == EdgeType.DISCORDANT:
+            return residual_cn.discordant[key.idx]
+        if key.type in (EdgeType.SOURCE, EdgeType.SINK):
+            return residual_cn.source[key.idx]
+        return None
+
+    for key in edge_keys:
+        edge_capacity = capacity(key)
+        if edge_capacity is None:
+            continue
+        for i in walks:
+            model.EdgeCapacity.add(
+                model.flow(key, i) <= edge_capacity * model.used(key, i)
+            )
+
+    for key in graph.structural_keys():
+        edge_capacity = capacity(key)
+        if edge_capacity is None:
+            continue
+        model.EdgeCapacity.add(
+            sum(model.flow(key, i) for i in walks) <= edge_capacity
+        )
+
+    for srci in range(len(graph.source_edges)):
+        source_key = EdgeId(EdgeType.SOURCE, srci)
+        sink_key = EdgeId(EdgeType.SINK, srci)
+
+        model.EdgeCapacity.add(
+            sum(
+                model.flow(source_key, i) + model.flow(sink_key, i)
+                for i in walks
+            )
+            <= residual_cn.source[srci]
+        )   
+    
+ 
  
 def add_weight_floor_constraint(
     model: CycleModel,
@@ -90,18 +130,28 @@ def add_weight_floor_constraint(
     """
     MIN_CYCLES: the solution must explain a fraction of the graph's CN.
     """
-    # TBD - this is not implemented in CE, please check CoRAL's implementation
-    #
-    #
+    explained_weight = sum(
+        model.flow(key, i) * len(graph.sequence_edges[key.idx])
+        for key in graph.sequence_keys()
+        for i in walks
+    )
+
+    fraction = 0.9
+    model.WeightFloor = pyo.Constraint(
+        expr = explained_weight >= fraction * graph.total_weights
+    )
  
  
 def add_unit_flow_constraints(
     model: CycleModel, graph: BreakpointGraph, walks: range
 ) -> None:
-    # TBD - see "3. copy number F with multiplicity one" in CE paper
-    #
-    #
- 
+    model.UnitFlow = pyo.ConstraintList()
+    big_m = graph.max_cn
+
+    for key in graph.edge_keys():
+        for i in walks:
+            model.UnitFlow.add(model.F[i] <= model.flow(key, i) + big_m * (1 - model.used(key, i)))
+
  
 def add_discordant_multiplicity_constraints(
     model: CycleModel, graph: BreakpointGraph, walks: range
@@ -109,28 +159,119 @@ def add_discordant_multiplicity_constraints(
     """
     On a discordant edge, f[u, v, i] == m * F[i] for some integer m.
     """
-    # TBD - see "4. discordant edge multiplicity constraint" in CE paper
-    #
-    #
+    model.DiscordantMultiplicity = pyo.ConstraintList()
+
+    C_max = graph.max_cn
+
+    for key in graph.structural_keys():
+        if key.type != EdgeType.DISCORDANT:
+            continue
+
+        multiplicities = range(
+            1, graph.multiplicity_bound(key) + 1
+        )
+
+        for i in walks:
+            model.DiscordantMultiplicity.add(
+                sum(
+                    model.mult_indicator(key, m, i)
+                    for m in multiplicities
+                )
+                == model.used(key, i)
+            )
+
+            for m in multiplicities:
+                model.DiscordantMultiplicity.add(
+                    model.mult_flow(key, m, i) <= m * model.F[i]
+                )
+
+                model.DiscordantMultiplicity.add(
+                    model.mult_flow(key, m, i) <= C_max * model.mult_indicator(key, m, i)
+                )
+
+                model.DiscordantMultiplicity.add(
+                    model.mult_flow(key, m, i) >= m * model.F[i] - C_max
+                    * (1 - model.mult_indicator(key, m, i))
+                )
+
+                model.DiscordantMultiplicity.add(
+                    model.mult_flow(key, m, i) >= 0
+                )
+
+            model.DiscordantMultiplicity.add(
+                model.flow(key, i)
+                == sum(
+                    model.mult_flow(key, m, i)
+                    for m in multiplicities
+                )
+            )
  
  
 def add_balance_constraints(
     model: CycleModel, graph: BreakpointGraph, walks: range
 ) -> None:
     """
-    At every node, sequence-edge flow equals breakpoint and terminal flow.
+    At every node, sequence-edge flow equals concordant and discordant flow.
     """
-    # TBD - see "2. balance" in CE paper, also need to consider MIN_CYCLES (for each i)
-    #
-    #
+    model.NodeBalance = pyo.ConstraintList()
+
+    for node, adjacency in graph.node_adjacencies.items():
+
+        sequence_keys = [
+            EdgeId(EdgeType.SEQUENCE, seqi)
+            for seqi in adjacency.sequence
+        ]
+
+        breakpoint_keys = [
+            EdgeId(EdgeType.CONCORDANT, ci)
+            for ci in adjacency.concordant
+        ]
+
+        breakpoint_keys.extend(
+            EdgeId(EdgeType.DISCORDANT, di)
+            for di in adjacency.discordant
+        )
+
+        for i in walks:
+            model.NodeBalance.add(
+                sum(
+                    model.flow(key, i)
+                    for key in breakpoint_keys
+                )
+                ==
+                sum(
+                    model.flow(key, i)
+                    for key in sequence_keys
+                )
+            )
  
  
 def add_source_constraint(
     model: CycleModel, graph: BreakpointGraph, walks: range
 ) -> None:
-    # TBD - two constraints
-    # 1. sum(x_sui) = sum(x_vti) (# source edges = # sink edges)
-    # 2. sum(x_sui) <= e[i] (if a walk exists, it is either a cycle or has only 1 source edge)
+
+    model.SourceConstraint = pyo.ConstraintList()
+
+    for i in walks:
+        model.SourceConstraint.add(
+            sum(
+                model.used(EdgeId(EdgeType.SOURCE, srci), i)
+                for srci in range(len(graph.source_edges))
+            )
+            ==
+            sum(
+                model.used(EdgeId(EdgeType.SINK, srci), i)
+                for srci in range(len(graph.source_edges))
+            )
+        )
+
+        model.SourceConstraint.add(
+            sum(
+                model.used(EdgeId(EdgeType.SOURCE, srci), i)
+                for srci in range(len(graph.source_edges))
+            )
+            <= model.e[i]
+        )
  
  
 def add_subpath_constraints(
@@ -143,12 +284,57 @@ def add_subpath_constraints(
     constraints = graph.longest_path_constraints
     if not constraints:
         return
- 
-    # TBD - please check CoRAL implementation
-    # CoRAL has a bug that subpath constraints were not considered
-    # you can require >= 0.5 (50%) subpath constraint satisfied by default in MIN_CYCLES.
-    # MAX_WEIGHT does not need R[p]. they can be doppped automatically by pyomo.
 
+    model.SubpathConstraints = pyo.ConstraintList()
+
+    for pi, path_constraint in enumerate(constraints):
+        for key, edge_count in path_constraint.edge_counts.items():
+            for i in walks:
+
+                if key.type == EdgeType.DISCORDANT:
+                    multiplicities = range(
+                        1,
+                        graph.multiplicity_bound(key) + 1
+                    )
+
+                    model.SubpathConstraints.add(
+                        sum(
+                            m * model.mult_indicator(key, m, i)
+                            for m in multiplicities
+                        )
+                        >= edge_count * model.r[pi, i]
+                    )
+
+                else:
+                    model.SubpathConstraints.add( 
+                        model.used(key, i) >= edge_count * model.r[pi, i] 
+                    )
+
+
+    if model_type == ModelType.MIN_CYCLES:
+
+        for pi in range(len(constraints)):
+
+            for i in walks:
+                model.SubpathConstraints.add(
+                    model.R[pi] >= model.r[pi, i]
+                )
+
+            model.SubpathConstraints.add(
+                sum(
+                    model.r[pi, i]
+                    for i in walks
+                )
+                >= model.R[pi]
+            )
+
+        model.SubpathConstraints.add(
+            sum(
+                model.R[pi]
+                for pi in range(len(constraints))
+            )
+            >= 0.5 * len(constraints)
+        )
  
  
 def add_connectivity_constraint(
@@ -157,9 +343,140 @@ def add_connectivity_constraint(
     if not graph.node_adjacencies:
         logger.debug("Empty node set; no connectivity rows to add.")
         return
- 
-    # TBD - may use https://link.springer.com/chapter/10.1007/3-540-36626-1_5
-    # can also use what is described in CE
-    # you can define any helper functions needed separately
+
+    model.Connectivity = pyo.ConstraintList()
+
+    s = graph.start_node
+    num_nodes = graph.num_nodes
+    structural_keys = list(graph.structural_keys())
+
+    def endpoints(key: EdgeId) -> tuple[Node, Node]:
+        if key.type == EdgeType.SEQUENCE:
+            edge = graph.sequence_edges[key.idx]
+            return edge.start_node, edge.end_node
+
+        if key.type == EdgeType.CONCORDANT:
+            edge = graph.concordant_edges[key.idx]
+            return edge.node1, edge.node2
+
+        if key.type == EdgeType.DISCORDANT:
+            edge = graph.discordant_edges[key.idx]
+            return edge.node1, edge.node2
+
+    edge_endpoints = {
+        key: endpoints(key)
+        for key in structural_keys
+    }
+
+    for i in walks:
+
+        model.Connectivity.add(
+            model.d_order(s, i) == 1
+        )
+  
+        incoming_s = []
+
+        for key in structural_keys:
+            u, v = edge_endpoints[key]
+
+            if s == u:
+                incoming_s.append(model.d_rev(key, i))
+
+            elif s == v:
+                incoming_s.append(model.d_fwd(key, i))
+
+        model.Connectivity.add(
+            sum(incoming_s) == 0
+        )
+
+        incident_s = [
+            key
+            for key in structural_keys
+            if s in edge_endpoints[key]
+        ]
+
+        selected_from_s = sum(
+            model.used(key, i)
+            for key in incident_s
+        )
+
+        for key in incident_s:
+            u, v = edge_endpoints[key]
+
+            if s == u:
+                outgoing = model.d_fwd(key, i)
+            else:
+                outgoing = model.d_rev(key, i)
+
+            model.Connectivity.add(
+                outgoing <= selected_from_s
+            )
+
+       
+        for node in graph.node_adjacencies:
+
+            incident_keys = [
+                key
+                for key in structural_keys
+                if node in edge_endpoints[key]
+            ]
+
+            model.Connectivity.add(
+                model.d_order(node, i)
+                <= num_nodes
+                * sum(
+                    model.used(key, i)
+                    for key in incident_keys
+                )
+            )
+
+       
+        for key in structural_keys:
+            model.Connectivity.add(
+                model.d_fwd(key, i) + model.d_rev(key, i) <= model.used(key, i)
+            )
+
+       
+        for node in graph.node_adjacencies:
+
+            if node == s:
+                continue
+
+            incoming = []
+            incident_keys = []
+
+            for key in structural_keys:
+                u, v = edge_endpoints[key]
+
+                if node == u:
+                    incident_keys.append(key)
+                    incoming.append(model.d_rev(key, i))
+
+                elif node == v:
+                    incident_keys.append(key)
+                    incoming.append(model.d_fwd(key, i))
+
+            model.Connectivity.add(
+                sum(incoming) 
+                >= (1 / num_nodes)
+                * sum(
+                    model.used(key, i)
+                    for key in incident_keys
+                )
+            )
+
+        
+        for key in structural_keys:
+            u, v = edge_endpoints[key]
+
+            model.Connectivity.add(
+                model.d_order(v, i)- model.d_order(u, i)
+                >= 1 - num_nodes * (1 - model.d_fwd(key, i))
+            )
+
+            model.Connectivity.add(
+                model.d_order(u, i) - model.d_order(v, i)
+                >= 1 - num_nodes * (1 - model.d_rev(key, i))
+            )
 
 
